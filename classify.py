252e68a4755d5cacbc88246c09b34c94a83f4936
Zhenghao Zhao <zhao.1980@osu.edu>
2019-05-06 21:37:54 -0400

    remove labeling on images, adding text output

--- classify.py
+++ classify.py
@@ -22,16 +22,19 @@ import os
 from edgetpu.classification.engine import ClassificationEngine
 import gstreamer
 
+
 def load_labels(path):
     p = re.compile(r'\s*(\d+)(.+)')
     with open(path, 'r', encoding='utf-8') as f:
-       lines = (p.match(line).groups() for line in f.readlines())
-       return {int(num): text.strip() for num, text in lines}
+        lines = (p.match(line).groups() for line in f.readlines())
+        return {int(num): text.strip() for num, text in lines}
+
 
 def generate_svg(dwg, text_lines):
     for y, line in enumerate(text_lines):
-      dwg.add(dwg.text(line, insert=(11, y*20+1), fill='black', font_size='20'))
-      dwg.add(dwg.text(line, insert=(10, y*20), fill='white', font_size='20'))
+        dwg.add(dwg.text(line, insert=(11, y*20+1), fill='black', font_size='20'))
+        dwg.add(dwg.text(line, insert=(10, y*20), fill='white', font_size='20'))
+
 
 def main():
     default_model_dir = "../all_models"
@@ -39,7 +42,7 @@ def main():
     default_labels = 'imagenet_labels.txt'
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir,default_model))
+                        default=os.path.join(default_model_dir, default_model))
     parser.add_argument('--labels', help='label file path',
                         default=os.path.join(default_model_dir, default_labels))
     parser.add_argument('--top_k', type=int, default=3,
@@ -48,27 +51,30 @@ def main():
                         help='class score threshold')
     args = parser.parse_args()
 
-    print("Loading %s with %s labels."%(args.model, args.labels))
+    print("Loading %s with %s labels." % (args.model, args.labels))
     engine = ClassificationEngine(args.model)
     labels = load_labels(args.labels)
 
     last_time = time.monotonic()
+
     def user_callback(image, svg_canvas):
-      nonlocal last_time
-      start_time = time.monotonic()
-      results = engine.ClassifyWithImage(image, threshold=args.threshold, top_k=args.top_k)
-      end_time = time.monotonic()
-      text_lines = [
-          'Inference: %.2f ms' %((end_time - start_time) * 1000),
-          'FPS: %.2f fps' %(1.0/(end_time - last_time)),
-      ]
-      for index, score in results:
-        text_lines.append('score=%.2f: %s' % (score, labels[index]))
-      print(' '.join(text_lines))
-      last_time = end_time
-      generate_svg(svg_canvas, text_lines)
+        nonlocal last_time
+        start_time = time.monotonic()
+        results = engine.ClassifyWithImage(
+            image, threshold=args.threshold, top_k=args.top_k)
+        end_time = time.monotonic()
+        text_lines = [
+            'Inference: %.2f ms' % ((end_time - start_time) * 1000),
+            'FPS: %.2f fps' % (1.0/(end_time - last_time)),
+        ]
+        for index, score in results:
+            text_lines.append('score=%.2f: %s' % (score, labels[index]))
+        print(' '.join(text_lines))
+        last_time = end_time
+        generate_svg(svg_canvas, text_lines)
 
     result = gstreamer.run_pipeline(user_callback)
+
 
 if __name__ == '__main__':
     main()
--- detect.py
+++ detect.py
@@ -34,15 +34,18 @@ import os
 from edgetpu.detection.engine import DetectionEngine
 import gstreamer
 
+
 def load_labels(path):
     p = re.compile(r'\s*(\d+)(.+)')
     with open(path, 'r', encoding='utf-8') as f:
-       lines = (p.match(line).groups() for line in f.readlines())
-       return {int(num): text.strip() for num, text in lines}
+        lines = (p.match(line).groups() for line in f.readlines())
+        return {int(num): text.strip() for num, text in lines}
+
 
 def shadow_text(dwg, x, y, text, font_size=20):
     dwg.add(dwg.text(text, insert=(x+1, y+1), fill='black', font_size=font_size))
     dwg.add(dwg.text(text, insert=(x, y), fill='white', font_size=font_size))
+
 
 def generate_svg(dwg, objs, labels, text_lines):
     width, height = dwg.attribs['width'], dwg.attribs['height']
@@ -51,20 +54,22 @@ def generate_svg(dwg, objs, labels, text_lines):
     for obj in objs:
         x0, y0, x1, y1 = obj.bounding_box.flatten().tolist()
         x, y, w, h = x0, y0, x1 - x0, y1 - y0
-        x, y, w, h = int(x * width), int(y * height), int(w * width), int(h * height)
+        x, y, w, h = int(x * width), int(y *
+                                         height), int(w * width), int(h * height)
         percent = int(100 * obj.score)
         label = '%d%% %s' % (percent, labels[obj.label_id])
         shadow_text(dwg, x, y - 5, label)
-        dwg.add(dwg.rect(insert=(x,y), size=(w, h),
-                        fill='red', fill_opacity=0.3, stroke='white'))
+        dwg.add(dwg.rect(insert=(x, y), size=(w, h),
+                         fill='red', fill_opacity=0.3, stroke='white'))
+
 
 def main():
-    default_model_dir = '../all_models'
-    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+    default_model_dir = 'models'
+    default_model = 'mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
     default_labels = 'coco_labels.txt'
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir,default_model))
+                        default=os.path.join(default_model_dir, default_model))
     parser.add_argument('--labels', help='label file path',
                         default=os.path.join(default_model_dir, default_labels))
     parser.add_argument('--top_k', type=int, default=3,
@@ -73,27 +78,40 @@ def main():
                         help='class score threshold')
     args = parser.parse_args()
 
-    print("Loading %s with %s labels."%(args.model, args.labels))
+    print("Loading %s with %s labels." % (args.model, args.labels))
     engine = DetectionEngine(args.model)
     labels = load_labels(args.labels)
 
     last_time = time.monotonic()
+
     def user_callback(image, svg_canvas):
-      nonlocal last_time
-      start_time = time.monotonic()
-      objs = engine.DetectWithImage(image, threshold=args.threshold,
-                                    keep_aspect_ratio=True, relative_coord=True,
-                                    top_k=args.top_k)
-      end_time = time.monotonic()
-      text_lines = [
-          'Inference: %.2f ms' %((end_time - start_time) * 1000),
-          'FPS: %.2f fps' %(1.0/(end_time - last_time)),
-      ]
-      print(' '.join(text_lines))
-      last_time = end_time
-      generate_svg(svg_canvas, objs, labels, text_lines)
+        nonlocal last_time
+        start_time = time.monotonic()
+        objs = engine.DetectWithImage(image, threshold=args.threshold,
+                                      keep_aspect_ratio=True, relative_coord=True,
+                                      top_k=args.top_k)
+        end_time = time.monotonic()
+        if objs:
+            print()
+        for obj in objs:
+            if labels:
+                print(labels[obj.label_id], 'score = ', obj.score)
+            else:
+                print('score = ', obj.score)
+            box = obj.bounding_box.flatten().tolist()
+            print(box)
+        else:
+            print('No object detected!')
+        text_lines = [
+            'Inference: %.2f ms' % ((end_time - start_time) * 1000),
+            'FPS: %.2f fps' % (1.0/(end_time - last_time)),
+        ]
+        print(' '.join(text_lines))
+        last_time = end_time
+        # generate_svg(svg_canvas, objs, labels, text_lines)
 
     result = gstreamer.run_pipeline(user_callback)
+
 
 if __name__ == '__main__':
     main()
