672eedb09473e50d2db08a5ed457b12458cc9a38
lcpdeb <47087196+lcpdeb@users.noreply.github.com>
2019-05-10 17:27:12 -0400

    Update closeCAN.py

--- closeCAN.py
+++ closeCAN.py
@@ -1,12 +1,13 @@
 from can.interfaces import slcan
+
 try:
- port = slcan.slcanBus("/dev/ttyACM0", bitrate=1000000)
- print("Closing CANBUS on ttyACM0...")
+    print("Closing CANBUS on ttyACM0...")
+    port = slcan.slcanBus("/dev/ttyACM0", bitrate=1000000)
 except:
- port = slcan.slcanBus("/dev/ttyACM1", bitrate=1000000)
- print("Can not find ttyACM0, closing ttyACM1...")
- port.close()
- print("CANBUS ttyACM1 Closed")
+    print("Can not find ttyACM0, closing ttyACM1...")
+    port = slcan.slcanBus("/dev/ttyACM1", bitrate=1000000)
+    port.close()
+    print("CANBUS ttyACM1 Closed")
 else:
- port.close()
- print("CANBUS ttyACMO Closed")
+    port.close()
+    print("CANBUS ttyACMO Closed")
