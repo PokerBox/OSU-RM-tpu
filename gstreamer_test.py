a59b7a7da6b5a88d387975bef501c33e460362f2
Zhenghao Zhao <zhao.1980@osu.edu>
2019-05-12 19:58:57 -0400

    Update gstreamer_test.py

--- gstreamer_test.py
+++ gstreamer_test.py
@@ -52,6 +52,7 @@ def on_new_sample(sink, screen_size, appsink_size, user_function):
     if result:
         img = Image.frombytes(
             'RGB', (appsink_size[0], appsink_size[1]), mapinfo.data, 'raw')
+        print(img.size)
         if ROTATE_180:
             img = img.rotate(180)
         # svg_canvas = svgwrite.Drawing(
