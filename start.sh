c8bb07e362febddafd76fea1a16e344476f96c28
PokerBox <zhaozhenghao7@gmail.com>
2019-05-27 16:23:12 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo Starting detect.py...
-python3 detect.py
 
 # Set the necessary environment variables - UBUNTU/UNITY/GNOME:
-export DISPLAY=$(w $(id -un) | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}')
+# export DISPLAY=$(w $(id -un) | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}')
+export DISPLAY=0
 export GNOME_DESKTOP_SESSION_ID=true
 export DBUS_SESSION_BUS_ADDRESS=$(sed -zne 's/^DBUS_SESSION_BUS_ADDRESS=//p' /proc/`pgrep gnome-session -U $(id -u)`/environ)
 
