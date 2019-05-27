ee47639235dcfd2cc4ff0fbb46b770edaf652ce2
PokerBox <zhaozhenghao7@gmail.com>
2019-05-27 16:22:39 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 echo Starting detect.py...
 python3 detect.py
+
+# Set the necessary environment variables - UBUNTU/UNITY/GNOME:
+export DISPLAY=$(w $(id -un) | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}')
+export GNOME_DESKTOP_SESSION_ID=true
+export DBUS_SESSION_BUS_ADDRESS=$(sed -zne 's/^DBUS_SESSION_BUS_ADDRESS=//p' /proc/`pgrep gnome-session -U $(id -u)`/environ)
+
+#Open new terminal and execute the script:
+/usr/bin/gnome-terminal -x sh -c 'cd /home/mendel/OSU-RM-tpu && python3 detect.py'
