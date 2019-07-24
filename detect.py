a22cf9a438c212d495218e91d267543d0c9c6e46
lcpdeb <47087196+lcpdeb@users.noreply.github.com>
2019-07-24 16:57:56 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -151,9 +151,9 @@ def choose_obj(objs, start_time):
 
 def main():
     default_model_dir = 'models'
-    default_model = '2019_05_13_whole(1)/output_tflite_graph_1557776948_edgetpu.tflite'
+    # default_model = '2019_05_13_whole(1)/output_tflite_graph_1557776948_edgetpu.tflite'
     # default_model = '2/output_tflite_graph_edgetpu.tflite'
-    # default_model = '3/output_tflite_graph_edgetpu.tflite'
+    default_model = '3/output_tflite_graph_edgetpu.tflite'
     default_labels = 'armor_plate_labels.txt'
     # default_model = 'mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
     # default_labels = 'face_labels.txt'
